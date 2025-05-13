0d6e963008fbf9d81e0cc187e74a39a6530501f0
RmRCunza <lacunzaramirez@gmail.com>
2025-05-13 13:42:28 -0500

    Agrego una clase

---
+++ Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba12121212
+{
+    class Class2
+    {
+    }
+}
