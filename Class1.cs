846b0b74274fd7627c37c8fbeff294162939d38d
RmRCunza <lacunzaramirez05@gmail.com>
2025-05-13 10:35:20 -0500

    Agrego una clase al programa

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba12121212
+{
+    class Class1
+    {
+        //Esto ya no es una prueba es un video
+    }
+}
