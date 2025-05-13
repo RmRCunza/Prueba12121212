05ec0e9f2e5e7f8d02f9f96103e80ee39c8788ac
RmRCunza <lacunzaramirez05@gmail.com>
2025-05-13 10:38:52 -0500

    He agregado dos lineas de codigo un WriteLine y un ReadLine

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Esto es una prueba");
+
+Console.WriteLine("Video de Git para Moanso");
+Console.ReadLine();
